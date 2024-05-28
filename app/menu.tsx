4a9cea711ec961f774749aea07e4462acefb58f1
joedubravec <josephdubravec@gmail.com>
2024-05-28 16:22:07 -0500

    fix menu links to be mobile-friendly

--- app/menu.tsx
+++ app/menu.tsx
@@ -32,7 +32,7 @@ export default function Menu() {
             )}
           >
             {/* <LinkIcon className="w-6" /> */}
-            <p className="hidden md:block">{link.name}</p>
+            <p>{link.name}</p>
           </Link>
         );
       })}
